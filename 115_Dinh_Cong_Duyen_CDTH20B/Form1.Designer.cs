a11c7914dd4a2c0786b6bcb59d738bd57a85a356
dinh duyen <duyendinh2021@outlook.com>
2022-04-13 21:39:35 +0700

    Add project files.

---
+++ 115_Dinh_Cong_Duyen_CDTH20B/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace _115_Dinh_Cong_Duyen_CDTH20B
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.btnBanmoi = new System.Windows.Forms.Button();
+            this.lblThanhtien2 = new System.Windows.Forms.Label();
+            this.lblTongtien = new System.Windows.Forms.Label();
+            this.lblTongsl = new System.Windows.Forms.Label();
+            this.lblTSL = new System.Windows.Forms.Label();
+            this.lblTb = new System.Windows.Forms.Label();
+            this.txtSoLuong = new System.Windows.Forms.TextBox();
+            this.form2 = new System.Windows.Forms.GroupBox();
+            this.lblTrungbinhLanban = new System.Windows.Forms.Label();
+            this.lblTTien = new System.Windows.Forms.Label();
+            this.lblThanhtien1 = new System.Windows.Forms.Label();
+            this.lblHoaDon1 = new System.Windows.Forms.Label();
+            this.lblSoluong = new System.Windows.Forms.Label();
+            this.grb1 = new System.Windows.Forms.GroupBox();
+            this.txtDonGia = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenuTinhTien = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuThemBan = new System.Windows.Forms.ToolStripMenuItem();
+            this.tínhTiềnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuThoat = new System.Windows.Forms.ToolStripMenuItem();
+            this.form2.SuspendLayout();
+            this.grb1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnThoat.ForeColor = System.Drawing.Color.Black;
+            this.btnThoat.Location = new System.Drawing.Point(504, 173);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(2);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(221, 59);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "&Thoát";
+            this.toolTip1.SetToolTip(this.btnThoat, "Nhấn Để Thoát");
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTinh.ForeColor = System.Drawing.Color.Black;
+            this.btnTinh.Location = new System.Drawing.Point(504, 101);
+            this.btnTinh.Margin = new System.Windows.Forms.Padding(2);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(221, 55);
+            this.btnTinh.TabIndex = 3;
+            this.btnTinh.Text = "&Tính";
+            this.toolTip1.SetToolTip(this.btnTinh, "Nhấn để tính");
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // btnBanmoi
+            // 
+            this.btnBanmoi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBanmoi.ForeColor = System.Drawing.Color.Black;
+            this.btnBanmoi.Location = new System.Drawing.Point(504, 24);
+            this.btnBanmoi.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBanmoi.Name = "btnBanmoi";
+            this.btnBanmoi.Size = new System.Drawing.Size(221, 54);
+            this.btnBanmoi.TabIndex = 3;
+            this.btnBanmoi.Text = "&Bàn Mới";
+            this.toolTip1.SetToolTip(this.btnBanmoi, "Nhấn Thêm bàn mới");
+            this.btnBanmoi.UseVisualStyleBackColor = true;
+            this.btnBanmoi.Click += new System.EventHandler(this.btnBanmoi_Click);
+            // 
+            // lblThanhtien2
+            // 
+            this.lblThanhtien2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblThanhtien2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblThanhtien2.ForeColor = System.Drawing.Color.Black;
+            this.lblThanhtien2.Location = new System.Drawing.Point(138, 172);
+            this.lblThanhtien2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblThanhtien2.Name = "lblThanhtien2";
+            this.lblThanhtien2.Size = new System.Drawing.Size(224, 29);
+            this.lblThanhtien2.TabIndex = 2;
+            // 
+            // lblTongtien
+            // 
+            this.lblTongtien.ForeColor = System.Drawing.Color.Black;
+            this.lblTongtien.Location = new System.Drawing.Point(13, 113);
+            this.lblTongtien.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTongtien.Name = "lblTongtien";
+            this.lblTongtien.Size = new System.Drawing.Size(137, 30);
+            this.lblTongtien.TabIndex = 2;
+            this.lblTongtien.Text = "Tổng Tiền";
+            this.lblTongtien.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lblTongsl
+            // 
+            this.lblTongsl.ForeColor = System.Drawing.Color.Black;
+            this.lblTongsl.Location = new System.Drawing.Point(13, 60);
+            this.lblTongsl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTongsl.Name = "lblTongsl";
+            this.lblTongsl.Size = new System.Drawing.Size(137, 30);
+            this.lblTongsl.TabIndex = 3;
+            this.lblTongsl.Text = "Tổng Số Lượng";
+            this.lblTongsl.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lblTSL
+            // 
+            this.lblTSL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblTSL.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTSL.ForeColor = System.Drawing.Color.Black;
+            this.lblTSL.Location = new System.Drawing.Point(215, 56);
+            this.lblTSL.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTSL.Name = "lblTSL";
+            this.lblTSL.Size = new System.Drawing.Size(222, 32);
+            this.lblTSL.TabIndex = 2;
+            // 
+            // lblTb
+            // 
+            this.lblTb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblTb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTb.ForeColor = System.Drawing.Color.Black;
+            this.lblTb.Location = new System.Drawing.Point(215, 158);
+            this.lblTb.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTb.Name = "lblTb";
+            this.lblTb.Size = new System.Drawing.Size(222, 33);
+            this.lblTb.TabIndex = 2;
+            // 
+            // txtSoLuong
+            // 
+            this.txtSoLuong.Location = new System.Drawing.Point(138, 34);
+            this.txtSoLuong.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSoLuong.Name = "txtSoLuong";
+            this.txtSoLuong.Size = new System.Drawing.Size(224, 27);
+            this.txtSoLuong.TabIndex = 1;
+            this.txtSoLuong.TextChanged += new System.EventHandler(this.txtSoLuong_TextChanged);
+            this.txtSoLuong.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSoLuong_KeyPress);
+            // 
+            // form2
+            // 
+            this.form2.Controls.Add(this.lblTrungbinhLanban);
+            this.form2.Controls.Add(this.lblTongtien);
+            this.form2.Controls.Add(this.lblTongsl);
+            this.form2.Controls.Add(this.lblTSL);
+            this.form2.Controls.Add(this.lblTb);
+            this.form2.Controls.Add(this.lblTTien);
+            this.form2.ForeColor = System.Drawing.Color.Red;
+            this.form2.Location = new System.Drawing.Point(26, 308);
+            this.form2.Margin = new System.Windows.Forms.Padding(2);
+            this.form2.Name = "form2";
+            this.form2.Padding = new System.Windows.Forms.Padding(2);
+            this.form2.Size = new System.Drawing.Size(770, 239);
+            this.form2.TabIndex = 3;
+            this.form2.TabStop = false;
+            this.form2.Text = "Thống kê";
+            // 
+            // lblTrungbinhLanban
+            // 
+            this.lblTrungbinhLanban.ForeColor = System.Drawing.Color.Black;
+            this.lblTrungbinhLanban.Location = new System.Drawing.Point(13, 165);
+            this.lblTrungbinhLanban.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTrungbinhLanban.Name = "lblTrungbinhLanban";
+            this.lblTrungbinhLanban.Size = new System.Drawing.Size(150, 29);
+            this.lblTrungbinhLanban.TabIndex = 1;
+            this.lblTrungbinhLanban.Text = "Trung Bình / Lần Bán";
+            this.lblTrungbinhLanban.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lblTTien
+            // 
+            this.lblTTien.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblTTien.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTTien.ForeColor = System.Drawing.Color.Black;
+            this.lblTTien.Location = new System.Drawing.Point(215, 104);
+            this.lblTTien.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTTien.Name = "lblTTien";
+            this.lblTTien.Size = new System.Drawing.Size(222, 37);
+            this.lblTTien.TabIndex = 2;
+            // 
+            // lblThanhtien1
+            // 
+            this.lblThanhtien1.ForeColor = System.Drawing.Color.Black;
+            this.lblThanhtien1.Location = new System.Drawing.Point(0, 173);
+            this.lblThanhtien1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblThanhtien1.Name = "lblThanhtien1";
+            this.lblThanhtien1.Size = new System.Drawing.Size(106, 29);
+            this.lblThanhtien1.TabIndex = 0;
+            this.lblThanhtien1.Text = "Thành Tiền";
+            this.lblThanhtien1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lblHoaDon1
+            // 
+            this.lblHoaDon1.ForeColor = System.Drawing.Color.Black;
+            this.lblHoaDon1.Location = new System.Drawing.Point(4, 100);
+            this.lblHoaDon1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblHoaDon1.Name = "lblHoaDon1";
+            this.lblHoaDon1.Size = new System.Drawing.Size(106, 30);
+            this.lblHoaDon1.TabIndex = 0;
+            this.lblHoaDon1.Text = "Đơn Giá";
+            this.lblHoaDon1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // lblSoluong
+            // 
+            this.lblSoluong.ForeColor = System.Drawing.Color.Black;
+            this.lblSoluong.Location = new System.Drawing.Point(4, 31);
+            this.lblSoluong.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSoluong.Name = "lblSoluong";
+            this.lblSoluong.Size = new System.Drawing.Size(83, 30);
+            this.lblSoluong.TabIndex = 0;
+            this.lblSoluong.Text = "Số Lượng";
+            this.lblSoluong.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // grb1
+            // 
+            this.grb1.Controls.Add(this.btnThoat);
+            this.grb1.Controls.Add(this.btnTinh);
+            this.grb1.Controls.Add(this.btnBanmoi);
+            this.grb1.Controls.Add(this.lblThanhtien2);
+            this.grb1.Controls.Add(this.txtDonGia);
+            this.grb1.Controls.Add(this.txtSoLuong);
+            this.grb1.Controls.Add(this.lblThanhtien1);
+            this.grb1.Controls.Add(this.lblHoaDon1);
+            this.grb1.Controls.Add(this.lblSoluong);
+            this.grb1.ForeColor = System.Drawing.Color.Red;
+            this.grb1.Location = new System.Drawing.Point(26, 47);
+            this.grb1.Margin = new System.Windows.Forms.Padding(2);
+            this.grb1.Name = "grb1";
+            this.grb1.Padding = new System.Windows.Forms.Padding(2);
+            this.grb1.Size = new System.Drawing.Size(770, 247);
+            this.grb1.TabIndex = 2;
+            this.grb1.TabStop = false;
+            this.grb1.Text = "Thông Tin Nhập";
+            // 
+            // txtDonGia
+            // 
+            this.txtDonGia.Location = new System.Drawing.Point(138, 106);
+            this.txtDonGia.Margin = new System.Windows.Forms.Padding(2);
+            this.txtDonGia.Name = "txtDonGia";
+            this.txtDonGia.Size = new System.Drawing.Size(224, 27);
+            this.txtDonGia.TabIndex = 7;
+            this.txtDonGia.TextChanged += new System.EventHandler(this.txtDonGia_TextChanged);
+            this.txtDonGia.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDonGia_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(0, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(821, 45);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CHƯƠNG TRÌNH BÁN HÀNG";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuTinhTien});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(821, 28);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MenuTinhTien
+            // 
+            this.MenuTinhTien.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuThemBan,
+            this.tínhTiềnToolStripMenuItem,
+            this.menuThoat});
+            this.MenuTinhTien.Name = "MenuTinhTien";
+            this.MenuTinhTien.Size = new System.Drawing.Size(60, 24);
+            this.MenuTinhTien.Text = "Menu";
+            // 
+            // MenuThemBan
+            // 
+            this.MenuThemBan.Name = "MenuThemBan";
+            this.MenuThemBan.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.B)));
+            this.MenuThemBan.Size = new System.Drawing.Size(239, 26);
+            this.MenuThemBan.Text = "Thêm Bàn Mới";
+            this.MenuThemBan.Click += new System.EventHandler(this.MenuThemBan_Click);
+            // 
+            // tínhTiềnToolStripMenuItem
+            // 
+            this.tínhTiềnToolStripMenuItem.Name = "tínhTiềnToolStripMenuItem";
+            this.tínhTiềnToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.T)));
+            this.tínhTiềnToolStripMenuItem.Size = new System.Drawing.Size(239, 26);
+            this.tínhTiềnToolStripMenuItem.Text = "Tính Tiền";
+            this.tínhTiềnToolStripMenuItem.Click += new System.EventHandler(this.tínhTiềnToolStripMenuItem_Click);
+            // 
+            // menuThoat
+            // 
+            this.menuThoat.Name = "menuThoat";
+            this.menuThoat.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.menuThoat.Size = new System.Drawing.Size(239, 26);
+            this.menuThoat.Text = "Thoát";
+            this.menuThoat.Click += new System.EventHandler(this.menuThoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 558);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.form2);
+            this.Controls.Add(this.grb1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Chương Trình Bán Hành";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.form2.ResumeLayout(false);
+            this.grb1.ResumeLayout(false);
+            this.grb1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ToolTip toolTip1;
+        private Button btnThoat;
+        private Button btnTinh;
+        private Button btnBanmoi;
+        private Label lblThanhtien2;
+        private Label lblTongtien;
+        private Label lblTongsl;
+        private Label lblTSL;
+        private Label lblTb;
+        private TextBox txtSoLuong;
+        private GroupBox form2;
+        private Label lblTrungbinhLanban;
+        private Label lblTTien;
+        private Label lblThanhtien1;
+        private Label lblHoaDon1;
+        private Label lblSoluong;
+        private GroupBox grb1;
+        private Label label1;
+        private TextBox txtDonGia;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem MenuTinhTien;
+        private ToolStripMenuItem MenuThemBan;
+        private ToolStripMenuItem tínhTiềnToolStripMenuItem;
+        private ToolStripMenuItem menuThoat;
+    }
+}
---
+++ 115_Dinh_Cong_Duyen_CDTH20B/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _115_Dinh_Cong_Duyen_CDTH20B
+{
+    public partial class Form1 : Form
+    {
+        Decimal donGia = 0;
+        Decimal soLuong = 0;
+        Decimal tongSoLong = 0;
+        Decimal tongTien = 0;
+        int lanBan = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSoLuong_TextChanged(object sender, EventArgs e)
+        {
+            //if (txtSoLuong.Text != "")
+            //{
+            //    try
+            //    {
+            //        Decimal a = int.Parse(txtSoLuong.Text.Trim());
+            //    }
+            //    catch
+            //    {
+            //        MessageBox.Show("Bạn Không Thể NHập Chử");
+            //        txtSoLuong.Clear();
+            //        txtSoLuong.Focus();
+            //    }
+            //}
+        }
+
+        private void txtDonGia_TextChanged(object sender, EventArgs e)
+        {
+            //if (txtDonGia.Text != "")
+            //{
+            //    try
+            //    {
+            //        Decimal a = int.Parse(txtSoLuong.Text.Trim());
+            //    }
+            //    catch
+            //    {
+            //        MessageBox.Show("Bạn Không Thể NHập Chử");
+            //        txtDonGia.Focus();
+            //        txtDonGia.Clear();
+            //    }
+            //}
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                soLuong = int.Parse(txtSoLuong.Text);
+                donGia = int.Parse(txtDonGia.Text);
+                Decimal thanhTien = soLuong * donGia;
+                lblThanhtien2.Text = thanhTien.ToString();
+                tongSoLong += soLuong;
+                tongTien += thanhTien;
+                lanBan++;
+
+                lblTSL.Text = tongSoLong.ToString();
+                lblTTien.Text = tongTien.ToString();
+                lblTb.Text = (tongTien / lanBan).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("bạn Chưa nhập giá trị. vui lòng nhập!");
+                txtSoLuong.Focus();
+            }
+        }
+
+        private void btnBanmoi_Click(object sender, EventArgs e)
+        {
+            txtSoLuong.Clear();
+            txtDonGia.Clear();
+            lblThanhtien2.Text = "";
+            lblTSL.Text = "";
+            lblTTien.Text = "";
+            lblTb.Text = "";
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("bạn muốn thoát chương trình ?", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void txtSoLuong_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar) == false)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtDonGia_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //if (char.IsDigit(e.KeyChar) == false && char.IsControl(e.KeyChar) == false)
+            //{
+            //    e.Handled = true;
+            //}
+            Regex regex = new Regex(@"[^a-zA-Z\s]");
+            if (regex.IsMatch(e.KeyChar.ToString()) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void MenuThemBan_Click(object sender, EventArgs e)
+        {
+            btnBanmoi_Click(sender, e);
+        }
+
+        private void tínhTiềnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            btnTinh_Click(sender, e);
+        }
+
+        private void menuThoat_Click(object sender, EventArgs e)
+        {
+            btnThoat_Click(sender,e);
+        }
+    }
+}
